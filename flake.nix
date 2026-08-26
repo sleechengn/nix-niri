6bbfa7e7760c72b4b7eae196f528e4ecf4c27721
lee <sleechengn@live.cn>
2026-08-26 20:50:09 +0800

    sav

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
             home-manager.useUserPackages = true;
             # 这里的 ryan 也得替换成你的用户名
             # 这里的 import 函数在前面 Nix 语法中介绍过了，不再赘述
-            home-manager.users.sa = import ./home.nix;
+            home-manager.users.sa = import ./sa.nix;
             home-manager.users.root = import ./root.nix;
             # 使用 home-manager.extraSpecialArgs 自定义传递给 ./home.nix 的参数
             # 取消注释下面这一行，就可以在 home.nix 中使用 flake 的所有 inputs 参数了
